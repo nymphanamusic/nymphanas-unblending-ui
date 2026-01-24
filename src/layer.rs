e89f94f77ee8c81bd964b8f21bb60b096301b51f
Nymphana <hi@nymphana.com>
2026-01-24 02:04:48 -0500

    feat: add theoretically complete UI code

--- src/layer.rs
+++ src/layer.rs
@@ -1,16 +1,45 @@
-use crate::unblending::unblending::BlendMode;
+use crate::unblending_ffi::unblending::BlendMode;
 use egui::ecolor::Hsva;
-use egui::{Color32, Widget};
+use egui::{Color32, ColorImage, ImageSource, TextureHandle, Widget};
+use std::hash::{Hash, Hasher};
+use uuid::Uuid;
 
+#[derive(Clone)]
 pub struct Layer {
+    pub uuid: Uuid,
     pub blend_mode: BlendMode,
     pub color: Color32,
-    pub variance: f32,
+    pub variance: f64,
+}
+
+impl PartialEq<Self> for Layer {
+    fn eq(&self, other: &Self) -> bool {
+        self.uuid == other.uuid
+    }
+}
+
+impl Eq for Layer {}
+
+impl Hash for Layer {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.uuid.hash(state)
+    }
+
+    fn hash_slice<H: Hasher>(data: &[Self], state: &mut H)
+    where
+        Self: Sized,
+    {
+        todo!()
+    }
 }
 
 impl Layer {
-    pub fn draw(&mut self, ui: &mut egui::Ui) {
-        egui::ComboBox::from_label("Take your pick")
+    pub fn draw(&mut self, ui: &mut egui::Ui, processed: Option<&TextureHandle>) {
+        if let Some(processed_texture) = processed {
+            ui.add(egui::Image::new(processed_texture).max_size([200.0, 400.0].into()));
+        };
+
+        egui::ComboBox::from_label("Blend mode")
             .selected_text(format!("{:?}", self.blend_mode.get_name()))
             .show_ui(ui, |ui| {
                 BlendMode::ALL.iter().for_each(|x| {
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 #![expect(rustdoc::missing_crate_level_docs)]
 
-use crate::unblending::unblending::BlendMode;
-use eframe::wgpu::TextureViewDescriptor;
-use eframe::{egui, egui_wgpu, Frame};
-use egui::load::TexturePoll;
-use egui::{Color32, SizeHint, TextureOptions, Vec2};
+use autocxx::prelude::Emplace;
+use autocxx::{as_copy, as_mov, c_int, WithinUniquePtr};
+use cxx::{CxxVector, UniquePtr};
+use eframe::{egui, Frame};
+use egui::load::ImagePoll;
+use egui::{Color32, ColorImage, SizeHint, TextureHandle, TextureOptions, Vec2};
 use layer::Layer;
 use rfd::FileDialog;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
+use std::pin::pin;
+use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
-use std::sync::{mpsc, Arc};
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
+use uuid::Uuid;
 
 mod layer;
 mod unblending;
+
+type ProcessedLayers = HashMap<Uuid, TextureHandle>;
+type StartProcess = (ImagePoll, Vec<Layer>);
+type FinishProcess = HashMap<Uuid, ColorImage>;
 
 fn main() -> eframe::Result {
     env_logger::init();
@@ -40,8 +48,11 @@ struct NymphanasUnblendingUI<'a> {
     image_path: Option<PathBuf>,
     image: Option<Box<egui::Image<'a>>>,
     layers: Vec<Layer>,
+    processed_layers: ProcessedLayers,
+
     processor_handle: Option<JoinHandle<Result<(), ()>>>,
     start_process_tx: Option<Sender<StartProcess>>,
+    finish_process_rx: Option<Receiver<FinishProcess>>,
 }
 
 impl Default for NymphanasUnblendingUI<'_> {
@@ -50,15 +61,18 @@ impl Default for NymphanasUnblendingUI<'_> {
             image_path: None,
             image: None,
             layers: vec![],
+            processed_layers: HashMap::new(),
             processor_handle: None,
             start_process_tx: None,
+            finish_process_rx: None,
         }
     }
 }
 
 impl eframe::App for NymphanasUnblendingUI<'_> {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut Frame) {
-        if let Some((start_process_tx, finish_process_rx)) = self.ensure_processor() {}
+        self.ensure_processor();
+        self.receive_processed_layers(&ctx);
 
         ctx.style_mut(|style| {
             style.spacing.interact_size = Vec2::new(60.0, 30.0);
@@ -69,7 +83,7 @@ impl eframe::App for NymphanasUnblendingUI<'_> {
             ui.heading("Nymphana's Unblending UI");
 
             if ui.button("Run unblending").clicked() {
-                self.handle_begin_processing(&ctx, &_frame);
+                self.handle_begin_processing(&ctx);
             }
 
             ui.horizontal(|ui| {
@@ -98,13 +112,16 @@ impl eframe::App for NymphanasUnblendingUI<'_> {
             // Layers
             if ui.button("Add layer").clicked() {
                 self.layers.push(Layer {
-                    blend_mode: BlendMode::Normal,
+                    uuid: Uuid::new_v4(),
+                    blend_mode: unblending::BlendMode::Normal,
                     color: Color32::from_gray(255),
                     variance: 0.5,
                 });
             }
             for (idx, layer) in self.layers.iter_mut().enumerate() {
-                ui.push_id(idx, |ui| layer.draw(ui));
+                ui.push_id(idx, |ui| {
+                    layer.draw(ui, self.processed_layers.get(&layer.uuid))
+                });
             }
             for (idx, layer) in self.layers.iter().enumerate() {
                 ui.label(format!(
@@ -132,27 +149,23 @@ impl NymphanasUnblendingUI<'_> {
         }
     }
 
-    fn handle_begin_processing(&mut self, ctx: &egui::Context, frame: &Frame) {
-        if let Some(image) = &self.image
+    fn handle_begin_processing(&mut self, ctx: &egui::Context) {
+        if let Some(image_path) = &self.image_path
             && let Some(start_process_tx) = &self.start_process_tx
-            && let Some(render_state) = frame.wgpu_render_state()
         {
-            let load_result = image.source(&ctx).load(
-                &ctx,
-                TextureOptions::default(),
-                SizeHint::Scale(1.0.into()),
-            );
-            if let Ok(load_poll) = load_result {
+            if let Ok(image_poll) =
+                ctx.try_load_image(image_path.to_str().unwrap(), SizeHint::Scale(1.0.into()))
+            {
                 start_process_tx
-                    .send((load_poll, render_state.renderer.clone()))
+                    .send((image_poll, self.layers.clone()))
                     .unwrap();
             }
         }
     }
 
-    fn ensure_processor(&mut self) -> Option<(Sender<StartProcess>, Receiver<FinishProcess>)> {
+    fn ensure_processor(&mut self) {
         if self.processor_handle.is_some() {
-            return None;
+            return;
         }
         let (start_process_tx, start_process_rx) = mpsc::channel();
         let (finish_process_tx, finish_process_rx) = mpsc::channel();
@@ -160,32 +173,72 @@ impl NymphanasUnblendingUI<'_> {
             processor(start_process_rx, finish_process_tx)
         }));
 
-        Some((start_process_tx, finish_process_rx))
+        self.start_process_tx = Some(start_process_tx);
+        self.finish_process_rx = Some(finish_process_rx);
+    }
+
+    fn receive_processed_layers(&mut self, ctx: &egui::Context) {
+        if let Some(finish_process_rx) = &self.finish_process_rx {
+            if let Ok(received) = finish_process_rx.try_recv() {
+                self.processed_layers =
+                    HashMap::from_iter(received.iter().map(|(&uuid, color_image)| {
+                        (
+                            uuid,
+                            ctx.load_texture(
+                                "processed.png",
+                                color_image.clone(),
+                                TextureOptions::LINEAR,
+                            ),
+                        )
+                    }))
+            }
+        }
     }
 }
-
-type StartProcess = (TexturePoll, Arc<egui::mutex::RwLock<egui_wgpu::Renderer>>);
-type FinishProcess = ();
 
 fn processor(
     start_process_rx: Receiver<StartProcess>,
     finish_process_tx: Sender<FinishProcess>,
 ) -> Result<(), ()> {
-    // (*_frame.wgpu_render_state().unwrap().renderer).read().texture()
-    for (image_poll, renderer) in start_process_rx {
-        while image_poll.is_pending() {
+    for (image_poll, layers) in start_process_rx {
+        while let ImagePoll::Pending { size: _ } = image_poll {
             thread::sleep(Duration::from_secs_f32(0.1))
         }
-        {
-            let read = renderer.read();
-            let texture = read.texture(&image_poll.texture_id().unwrap());
-            if let Some(texture) = texture
-                && let Some(texture) = &texture.texture
-            {
-                let view = texture.create_view(&TextureViewDescriptor::default());
-                view.texture().dimension()
-            }
-        }
+        let ImagePoll::Ready { image } = image_poll else {
+            panic!("ImagePoll stopped pending but had no Ready")
+        };
+        finish_process_tx.send(do_process(&image, &layers));
     }
     Ok(())
+}
+
+fn do_process<'a>(image: &ColorImage, layers: &Vec<Layer>) -> HashMap<Uuid, ColorImage> {
+    let color_image = unblending_ffi::color_image_from_pixels(&image.pixels, image.size);
+    let mut layer_infos = CxxVector::<unblending::LayerInfo>::new();
+    for layer in layers {
+        let mut info = unblending_helpers::make_layer_info(
+            unblending::CompOp::SourceOver().within_unique_ptr(),
+            layer.blend_mode.clone(),
+            layer.color.r() as f64 / 255.0,
+            layer.color.g() as f64 / 255.0,
+            layer.color.b() as f64 / 255.0,
+            layer.variance,
+        )
+        .within_unique_ptr();
+        unblending_helpers::push_layer_info(layer_infos.pin_mut(), info.pin_mut());
+    }
+
+    HashMap::from_iter(
+        layers.iter().map(|x| x.uuid).zip(
+            unblending::compute_color_unmixing(&color_image, &layer_infos, true, c_int(12))
+                .pin_mut()
+                .iter_mut()
+                .map(|x| {
+                    ColorImage::new(
+                        image.size,
+                        unblending_ffi::pixels_from_color_image(x, image.size),
+                    )
+                }),
+        ),
+    )
 }
